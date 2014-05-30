3ec23e6e1892d9ef39881ff17644813482b93f74
Federico Marani <flagzeta@gmail.com>
2014-05-30 08:41:12 +0100

    first working websocket test!

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -4,6 +4,9 @@ sys.path.append("..")
 from unittest import mock
 from tornado.testing import AsyncHTTPTestCase, gen_test
 from tornado import websocket
+from tornado.ioloop import IOLoop
+import time
+import json
 
 from app import TunnelChat
 
@@ -14,23 +17,29 @@ class ChatTest(AsyncHTTPTestCase):
     def get_protocol(self):
         return "ws"
 
+    def get_new_ioloop(self):
+        return IOLoop.instance()  # the default Client loop
+
     @gen_test
     def test_room_registry(self):
         url = "/chatsocket"
 
         with mock.patch("handlers.chat.ChatSocketHandler.get_current_user") as get_user:
             get_user.return_value = {'name': 'User 1'}
-            connection1 = yield websocket.websocket_connect(
-                self.get_url(url),
-                io_loop=self.io_loop)
+            connection1future = websocket.websocket_connect(self.get_url(url), io_loop=self.io_loop)
+            connection1 = yield connection1future
 
         with mock.patch("handlers.chat.ChatSocketHandler.get_current_user") as get_user:
             get_user.return_value = {'name': 'User 2'}
-            connection2 = yield websocket.websocket_connect(
-                self.get_url(url),
-                io_loop=self.io_loop)
+            connection2 = yield websocket.websocket_connect(self.get_url(url), io_loop=self.io_loop)
 
         message = yield connection1.read_message()
+        jmessage = json.loads(message)
+        self.assertEqual('joined the chat', jmessage['body'])
+        self.assertEqual('User 2', jmessage['from'])
+        """
         message = yield connection2.read_message()
-
-        self.assertEqual('2 just joined the room', message)
+        jmessage = json.loads(message)
+        self.assertEqual('joined the chat', jmessage['body'])
+        self.assertEqual('User 1', jmessage['from'])
+        """
